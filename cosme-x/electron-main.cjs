11019648ebba341e420db838ee8d1bce855b1301
Szyoo <losiner.y@gmail.com>
2025-06-02 16:07:49 +0900

    first commit

---
+++ cosme-x/electron-main.cjs
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  })
+  // 开发时加载本地 Vite 服务，正式打包时改成文件路径
+  win.loadURL('http://localhost:5173/')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
---
+++ cosme-x/src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import { api } from './api'
+
+const prizes = ref([])
+
+onMounted(() => {
+  api.get('/prizes')
+    .then(res => {
+      prizes.value = res.data
+    })
+    .catch(err => {
+      console.error('Failed to fetch prizes:', err)
+    })
+})
+</script>
+
+<template>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+  <div class="text-3xl text-blue-500 font-bold">Hello Tailwind!</div>
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
